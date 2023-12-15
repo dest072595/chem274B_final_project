885864c5bdd08987c669636d45c2608ae09de9c4
AUSTIN GLOVER <AUSTIN_GLOVER@BERKELEY.EDU>
2023-12-15 01:22:01 -0800

    working periodic and fixed boundary conditions

--- Include/Cellular_Automata.h
+++ Include/Cellular_Automata.h
@@ -6,6 +6,7 @@
 #include <exception>
 #include <format> 
 #include <iostream>
+#include <map> 
 
 //size_t rows, size_t cols,
 template <typename T, size_t n_rows, size_t n_cols> 
@@ -13,23 +14,33 @@ class Cellular_Automata {
     private: 
         std::vector<std::vector<T>> _hidden_state; 
         std::function< T (const std::array<std::array<T, n_cols>, n_rows>)> _updateRule;
-        std::function<void (std::vector<std::vector<T>>& state, size_t rpad, size_t cpad)> _outOfBoundsRule; 
+        std::function<void (std::vector<std::vector<T>>& state, int rpad, int cpad)> _outOfBoundsRule; 
         
         size_t _step; 
-        size_t _rpad; 
-        size_t _cpad;
+        int _rpad; 
+        int _cpad;
 
-        const std::array<std::array<T, n_rows>, n_cols> get_neighborhood_around(size_t row, size_t col)
+        const std::array<std::array<T, n_cols>, n_rows> get_neighborhood_around(int row, int col)
         {
             // empty initialization
-            std::array<std::array<T, n_rows>, n_cols> n {};
-
-            for(size_t i = 0; i < n_rows; i++){
-                for(size_t j = 0; j < n_cols; j++){
-                    n.at(i).at(j) = _hidden_state.at(row + i - _rpad).at(col + j - _cpad); 
+            
+            std::array<std::array<T, n_cols>, n_rows> neighborhood {};
+            
+            // std::cout << "_rpad: " << _rpad << std::endl;
+            // std::cout << "_cpad: " << _cpad << std::endl;
+            // std::cout << "n_row: " << n_rows << std::endl;
+            // std::cout << "n_col: " << n_cols << std::endl;
+            // std::cout << "row: " << row << std::endl;
+            // std::cout << "col: " << col << std::endl;
+            for(int i = - _rpad ; i <= _rpad; i++){
+                for(int j = -_cpad ; j <= _cpad; j++){
+                    // std::cout << "i: " << i << std::endl;
+                    // std::cout << "j: " << j << std::endl;
+                    
+                    neighborhood.at(i + _rpad).at(j + _cpad) = _hidden_state.at(row + i).at(col + j); 
                 }
             }
-            return n; 
+            return neighborhood; 
         }
     public: 
         Cellular_Automata(
@@ -96,10 +107,11 @@ class Cellular_Automata {
         void update(){
             // Initialize Empty State
             std::vector<std::vector<T>> next_hidden_state (_hidden_state.size(),std::vector<T>(_hidden_state.at(0).size())); 
-            
+            //std::cout << "hidden state size " << _hidden_state.size() << std::endl; 
+            //std::cout << "hidden state.at(0) size " << _hidden_state.at(0).size() << std::endl; 
             // Calculate New Inbounds State  
-            for(size_t i = _rpad; i < _hidden_state.size() - _rpad; i++){
-            for(size_t j = _rpad; j < _hidden_state.at(0).size() - _cpad; j++){
+            for(int i = _rpad; i < _hidden_state.size() - _rpad; i++){
+            for(int j = _cpad; j < _hidden_state.at(0).size() - _cpad; j++){
                 next_hidden_state.at(i).at(j) = _updateRule(get_neighborhood_around(i,j));
             }
             }
@@ -130,5 +142,127 @@ class Cellular_Automata {
             }
             return out_state; 
         }
+
+        auto gimme(){
+            return _hidden_state; 
+        }
     
 }; 
+
+// helper function for copying around data in a std::vector<std::vector<T>> 
+template<typename T> 
+void copy_data_around(
+    std::vector<std::vector<T>>& data, 
+    int source_row, 
+    int source_col, 
+    int dest_row, 
+    int dest_col, 
+    int num_rows_to_copy, 
+    int num_cols_to_copy
+    )
+{
+    for(int i = 0; i < num_rows_to_copy; i++){
+    for(int j = 0; j < num_cols_to_copy; j++){
+        data.at(dest_row + i).at(dest_col + j) = data.at(source_row + i).at(source_col + j); 
+    }
+    }
+}
+
+// Periodic Boundary Condition Function, Automatically makes an update rule for a given typename T
+template<typename T>
+std::function<void (std::vector<std::vector<T>>& state, int rpad, int cpad)> PeriodicBoundary(){
+    return [](std::vector<std::vector<T>>& state, size_t rpad, size_t cpad){
+        int rows = state.size();
+        int cols = state.at(0).size(); 
+
+        int start_row = rpad;
+        int start_col = cpad;
+        int end_row = rows - rpad;
+        int end_col = cols - cpad;
+
+        // Copy data to the top padding
+        copy_data_around(state, start_row, start_col, 0, start_col, rpad, end_col - start_col);
+
+        // Copy data to the bottom padding
+        copy_data_around(state, start_row, start_col, rows - rpad, start_col, rpad, end_col - start_col);
+
+        // Copy data to the left padding
+        copy_data_around(state, start_row, start_col, start_row, 0, end_row - start_row, cpad);
+
+        // Copy data to the right padding
+        copy_data_around(state, start_row, start_col, start_row, cols - cpad, end_row - start_row, cpad);
+
+        // Copy data to the top-left corner
+        copy_data_around(state, start_row, start_col, 0, 0, rpad, cpad);
+
+        // Copy data to the top-right corner
+        copy_data_around(state, start_row, end_col - cpad, 0, cols - cpad, rpad, cpad);
+
+        // Copy data to the bottom-left corner
+        copy_data_around(state, end_row - rpad, start_col, rows - rpad, 0, rpad, cpad);
+
+        // Copy data to the bottom-right corner
+        copy_data_around(state, end_row - rpad, end_col - cpad, rows - rpad, cols - cpad, rpad, cpad);    
+    };
+}
+
+// helper function for filling data in a std::vector<std::vector<T>> 
+template<typename T> 
+void fill_data(
+    std::vector<std::vector<T>>& data,
+    const T value, 
+    int dest_row, 
+    int dest_col, 
+    int num_rows_to_copy, 
+    int num_cols_to_copy
+    )
+{
+    for(int i = 0; i < num_rows_to_copy; i++){
+    for(int j = 0; j < num_cols_to_copy; j++){
+        data.at(dest_row + i).at(dest_col + j) = value; 
+    }
+    }
+}
+
+// Periodic Boundary Condition Function, Automatically makes an update rule for a given typename T
+template<typename T>
+std::function<void (std::vector<std::vector<T>>& state, int rpad, int cpad)> FixedBoundary(const T value){
+    return [value](std::vector<std::vector<T>>& state, size_t rpad, size_t cpad){
+        int rows = state.size();
+        int cols = state.at(0).size(); 
+
+        int start_row = rpad;
+        int start_col = cpad;
+        int end_row = rows - rpad;
+        int end_col = cols - cpad;
+
+        // Copy data to the top padding
+        fill_data(state, value, 0, start_col, rpad, end_col - start_col);
+
+        // Copy data to the bottom padding
+        fill_data(state, value, rows - rpad, start_col, rpad, end_col - start_col);
+
+        // Copy data to the left padding
+        fill_data(state, value, start_row, 0, end_row - start_row, cpad);
+
+        // Copy data to the right padding
+        fill_data(state, value, start_row, cols - cpad, end_row - start_row, cpad);
+
+        // Copy data to the top-left corner
+        fill_data(state, value, 0, 0, rpad, cpad);
+
+        // Copy data to the top-right corner
+        fill_data(state, value, 0, cols - cpad, rpad, cpad);
+
+        // Copy data to the bottom-left corner
+        fill_data(state, value, rows - rpad, 0, rpad, cpad);
+
+        // Copy data to the bottom-right corner
+        fill_data(state, value, rows - rpad, cols - cpad, rpad, cpad);    
+    };
+}
+
+template<typename T>
+std::function<void (std::vector<std::vector<T>>& state, size_t rpad, size_t cpad)> StraightConditionalTransition(const std::map<T,T>& value){
+    return [](std::vector<std::vector<T>>& state, size_t rpad, size_t cpad){};
+}
--- Tests/CA_Test.cpp
+++ Tests/CA_Test.cpp
@@ -4,7 +4,7 @@ struct Person{
     char health = 'h'; 
 };
 
-void print_state(std::vector<std::vector<Person>> state){
+void print_state(const std::vector<std::vector<Person>>& state){
     for(auto vec : state){
         for(auto p : vec){
             std::cout << p.health << " "; 
@@ -20,18 +20,28 @@ int main(){
     // columns of the inital state
     constexpr size_t cols = 3; 
     // rows in the neighborhood
-    constexpr size_t n_rows = 3; 
+    constexpr size_t n_rows = 1; 
     // columns in the neighborhood
-    constexpr size_t n_cols  = 3; 
+    constexpr size_t n_cols  = 1; 
 
     // create a 4 row by 5 column initial state
     std::vector<std::vector<Person>> initial_state (rows, std::vector<Person>(cols, Person())); 
 
     // create update rule 
         // This rule replaces everyone with a 'c' person
-    std::function<Person (const std::array<std::array<Person, n_cols>, n_rows>&) > updateRule = [](const std::array<std::array<Person, n_cols>, n_rows>& arr)-> Person 
-    { 
-        Person p = arr.at(1).at(1);
+    std::function<Person (const std::array<std::array<Person, n_cols>, n_rows>) > updateRule = [](const std::array<std::array<Person, n_cols>, n_rows>& neighborhood)-> Person 
+    {   
+        // print out 
+        // for(auto vec : neighborhood){
+        //     for(auto p : vec){
+        //         std::cout << p.health << " ";
+        //     }
+        //     std::cout << std::endl; 
+        // }
+        // std::cout<< std::endl;
+        
+        Person p = neighborhood.at((n_rows - 1) /2).at((n_cols - 1) /2);
+    
         Person r; 
         if (p.health == 'h')
             r.health = 'c';
@@ -44,9 +54,8 @@ int main(){
 
     // create boundary rule 
         // This rule does nothing
-    std::function<void(std::vector<std::vector<Person>>&, size_t, size_t)> outOfBoundsRule = [](std::vector<std::vector<Person>>& state, size_t rpad, size_t cpad)->void{};
-
-  
+    //std::function<void(std::vector<std::vector<Person>>&, size_t, size_t)> outOfBoundsRule = [](std::vector<std::vector<Person>>& state, size_t rpad, size_t cpad)->void{};
+    auto outOfBoundsRule = FixedBoundary<Person>(Person{'x'});
 
     Cellular_Automata<Person, n_rows, n_cols> CAP (
         initial_state, 
@@ -58,15 +67,12 @@ int main(){
     print_state(initial_state);
 
     CAP.update(); 
-
     print_state(CAP.getState()); 
+    print_state(CAP.gimme()); 
 
     CAP.update(); 
-
     print_state(CAP.getState()); 
-
-    CAP.update(); 
-
-    print_state(CAP.getState()); 
+    print_state(CAP.gimme()); 
+    
     return 0; 
 }
